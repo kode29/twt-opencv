f2e83b50ebb841499d8f1e053db6a3e5f6e9902e
Kyle Perkins <kyperkins@gmail.com>
2023-12-17 15:23:28 -0500

    finish tutorial5

--- tutorial5.py
+++ tutorial5.py
@@ -9,8 +9,15 @@ while True:
     height = int(cap.get(4))
     
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([90,50,50])
+    upper_blue = np.array([130,255,255])
     
-    cv2.imshow('frame', frame)
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+    
+    cv2.imshow('frame',result)
+#     cv2.imshow('mask',mask)
     
     if cv2.waitKey(1) == ord('q'):
         break
